8e2d61970b500c817d5b8c5a3799f99a80242f38
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-10 10:04:52 +0100

    add backend

---
+++ backend/radio-life/src/main/java/dev/radio_life/RadioLifeApplication.java
@@ -0,0 +1,13 @@
+package dev.radio_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioLifeApplication.class, args);
+	}
+
+}
---
+++ backend/radio-life/src/test/java/dev/radio_life/RadioLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.radio_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
