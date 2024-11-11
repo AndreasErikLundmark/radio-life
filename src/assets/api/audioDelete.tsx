90dbf188f0a0b852808bec3d68144d558704848d
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-11 14:32:17 +0100

    backend .env

--- src/assets/api/audioDelete.tsx
+++ src/assets/api/audioDelete.tsx
@@ -1,8 +1,7 @@
 import { AudioFile } from "./types";
 
 const DELETE_URL = import.meta.env.VITE_RADIO_LIFE_DELETE_URL;
-// const GET_URL = "http://localhost:3000/radiolife/";
-
+console.log("delete url: ", DELETE_URL);
 export const AudioFileDelete = async (fileName: string): Promise<any> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(DELETE_URL + fileName, {
